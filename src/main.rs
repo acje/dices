083ba3c48b945ba58054119994e7eec55ffd1c19
Anders Jensen <anders.c.jensen@gmail.com>
2021-01-20 14:38:03 +0100

    add bytecount method and crate

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ struct Cnt {
 fn main() {
     // Roll some D6 dices
     let mut rng = rand::thread_rng();
-    let dices: Vec<u8> = (0..200_000_000)
+    let dices: Vec<u8> = (0..100_000_000)
         .map(|_| Uniform::from(1..=6).sample(&mut rng))
         .collect();
 
@@ -42,6 +42,16 @@ fn main() {
     println!("fives: {}", dices.iter().filter(|&x| *x == 5).count());
     println!("sixs: {}", dices.iter().filter(|&x| *x == 6).count());
     println!("multiscan fun count {:.1?}", time.elapsed());
+
+    // Bytecount crate
+    let time = Instant::now();
+    println!("bytecount ones:{}", bytecount::count(&dices, 1u8));
+    println!("bytecount twos:{}", bytecount::count(&dices, 2u8));
+    println!("bytecount threes:{}", bytecount::count(&dices, 3u8));
+    println!("bytecount fours:{}", bytecount::count(&dices, 4u8));
+    println!("bytecount fives:{}", bytecount::count(&dices, 5u8));
+    println!("bytecount sixs:{}", bytecount::count(&dices, 6u8));
+    println!("bytecount {:.1?}", time.elapsed());
 }
 
 fn imp_dice_count(dices: &[u8]) -> Cnt {
@@ -118,85 +128,87 @@ fn par_fun_dice_count(dices: &[u8]) -> Cnt {
                 sixs: 1,
                 ..Cnt::default()
             },
-            _ => {Cnt::default()},
+            _ => Cnt::default(),
         })
         .reduce(
             || Cnt::default(),
-            |cnt, n| -> Cnt {match n {
-                Cnt {
-                    ones: 1,
-                    twos: 0,
-                    threes: 0,
-                    fours: 0,
-                    fives: 0,
-                    sixs: 0,
-                } => Cnt {
-                    ones: cnt.ones + 1,
-                    ..cnt
-                },
-                Cnt {
-                    ones: 0,
-                    twos: 1,
-                    threes: 0,
-                    fours: 0,
-                    fives: 0,
-                    sixs: 0,
-                } => Cnt {
-                    twos: cnt.twos + 1,
-                    ..cnt
-                },
-                Cnt {
-                    ones: 0,
-                    twos: 0,
-                    threes: 1,
-                    fours: 0,
-                    fives: 0,
-                    sixs: 0,
-                } => Cnt {
-                    threes: cnt.threes + 1,
-                    ..cnt
-                },
-                Cnt {
-                    ones: 0,
-                    twos: 0,
-                    threes: 0,
-                    fours: 1,
-                    fives: 0,
-                    sixs: 0,
-                } => Cnt {
-                    fours: cnt.fours + 1,
-                    ..cnt
-                },
-                Cnt {
-                    ones: 0,
-                    twos: 0,
-                    threes: 0,
-                    fours: 0,
-                    fives: 1,
-                    sixs: 0,
-                } => Cnt {
-                    fives: cnt.fives + 1,
-                    ..cnt
-                },
-                Cnt {
-                    ones: 0,
-                    twos: 0,
-                    threes: 0,
-                    fours: 0,
-                    fives: 0,
-                    sixs: 1,
-                } => Cnt {
-                    sixs: cnt.sixs + 1,
-                    ..cnt
-                },
-                _ => Cnt {
-                    ones: cnt.ones + n.ones,
-                    twos: cnt.twos + n.twos,
-                    threes: cnt.threes + n.threes,
-                    fours: cnt.fours + n.fours,
-                    fives: cnt.fives + n.fives,
-                    sixs: cnt.sixs + n.sixs,
-                },
-            }},
+            |cnt, n| -> Cnt {
+                match n {
+                    Cnt {
+                        ones: 1,
+                        twos: 0,
+                        threes: 0,
+                        fours: 0,
+                        fives: 0,
+                        sixs: 0,
+                    } => Cnt {
+                        ones: cnt.ones + 1,
+                        ..cnt
+                    },
+                    Cnt {
+                        ones: 0,
+                        twos: 1,
+                        threes: 0,
+                        fours: 0,
+                        fives: 0,
+                        sixs: 0,
+                    } => Cnt {
+                        twos: cnt.twos + 1,
+                        ..cnt
+                    },
+                    Cnt {
+                        ones: 0,
+                        twos: 0,
+                        threes: 1,
+                        fours: 0,
+                        fives: 0,
+                        sixs: 0,
+                    } => Cnt {
+                        threes: cnt.threes + 1,
+                        ..cnt
+                    },
+                    Cnt {
+                        ones: 0,
+                        twos: 0,
+                        threes: 0,
+                        fours: 1,
+                        fives: 0,
+                        sixs: 0,
+                    } => Cnt {
+                        fours: cnt.fours + 1,
+                        ..cnt
+                    },
+                    Cnt {
+                        ones: 0,
+                        twos: 0,
+                        threes: 0,
+                        fours: 0,
+                        fives: 1,
+                        sixs: 0,
+                    } => Cnt {
+                        fives: cnt.fives + 1,
+                        ..cnt
+                    },
+                    Cnt {
+                        ones: 0,
+                        twos: 0,
+                        threes: 0,
+                        fours: 0,
+                        fives: 0,
+                        sixs: 1,
+                    } => Cnt {
+                        sixs: cnt.sixs + 1,
+                        ..cnt
+                    },
+                    _ => Cnt {
+                        ones: cnt.ones + n.ones,
+                        twos: cnt.twos + n.twos,
+                        threes: cnt.threes + n.threes,
+                        fours: cnt.fours + n.fours,
+                        fives: cnt.fives + n.fives,
+                        sixs: cnt.sixs + n.sixs,
+                    },
+                }
+            },
         )
 }
